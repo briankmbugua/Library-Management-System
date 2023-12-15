1fb5bb2710a53bae4bc30a3cba5c5f1655c7fc29
briankmbugua <briankmbuguak@gmail.com>
2023-12-16 00:00:16 +0300

    the middlewares folder

---
+++ middlewares/libraryMiddleware.js
@@ -0,0 +1,23 @@
+const LibrariesModel = require('../models/librariesModel');
+
+
+// Middleware to handle dynamic ID
+async function libraryParamMiddleware(req, res, next, id) {
+    try {
+        const library = await LibrariesModel.getLibraryById(id);
+
+        if (library) {
+            req.library = library;
+            next();
+        } else {
+            res.status(404).json({ error: 'Library not found' });
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal Server Error While getting Library by Id' })
+    }
+}
+
+module.exports = {
+    libraryParamMiddleware,
+}
