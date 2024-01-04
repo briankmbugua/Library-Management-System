39e3ca6e027a234195027e11727f6ecf0962754a
briankmbuguak <briankmbuguak@gmail.com>
2024-01-05 02:11:43 +0300

    librarianRegister route

--- routes/librariansRegister.js
+++ routes/librariansRegister.js
@@ -1,6 +1,7 @@
 let express = require('express');
 let router = express.Router();
 let UsersModel = require('../models/usersModel');
+let librariesModel = require('../models/librariesModel');
 //Librarian Registration
 router.post('/', async (req, res) => {
     const {
@@ -10,10 +11,11 @@ router.post('/', async (req, res) => {
         role,
         library_name,
         library_address,
-        phone_number,
+        library_phone_number,
         library_email
     } = req.body;
     console.log(req.body);
+    UsersModel.registerUser(username,password,email, role,library_name, library_address, library_phone_number, library_email);
 });
 
 module.exports = router;
