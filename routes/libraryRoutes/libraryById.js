47fc70f3a6356428a3713b8e8cc438016b574c86
briankmbuguak <briankmbuguak@gmail.com>
2024-01-13 17:11:15 +0300

    renamed libraryID to id

--- routes/libraryRoutes/libraryById.js
+++ routes/libraryRoutes/libraryById.js
@@ -1,14 +1,24 @@
 let express = require('express');
 const { libraryParamMiddleware } = require('../../middlewares/libraryMiddleware');
+const authmiddleware = require('../../middlewares/authmiddleware');
 let router = express.Router();
 
 
 // Use the libraryParamMiddleWare for handling dynamic ID
 
-router.param('id', libraryParamMiddleware);
+router.param('id',libraryParamMiddleware);
 
-router.get('/:id', (req, res) => {
+router.get('/:id', [authmiddleware,(req, res) => {
+    const { id } = req.params;
+    const librarianLibraryId = req.user.library.id;
+    console.log(id);
+    console.log(librarianLibraryId);
+    if (id == librarianLibraryId) {
+        res.json(req.library);
+    } else {
+        res.send('Not authorized');
+    }
     res.json(req.library);
-})
+}])
 
 module.exports = router;
