f75637299b8fd4ca5386ada87605e7c86b8697b9
briankmbuguak <briankmbuguak@gmail.com>
2024-01-13 17:13:39 +0300

    made updateLibraryDetails to be librarian specific

--- routes/libraryRoutes/updateLibrary.js
+++ routes/libraryRoutes/updateLibrary.js
@@ -1,8 +1,9 @@
-let express = require('express');
-let router = express.Router();
-let librariesModel = require('../../models/librariesModel');
+const express = require('express');
+const router = express.Router();
+const librariesModel = require('../../models/librariesModel');
+const authmiddleware = require('../../middlewares/authmiddleware');
 // Update Library
-router.put('/:libraryId', async (req, res) => {
+router.put('/:libraryId', authmiddleware, async (req, res) => {
     const libraryId = req.params.libraryId;
     const updatedDetails = req.body; // Assuming you're sending updated details in the request body
     const {
@@ -12,18 +13,23 @@ router.put('/:libraryId', async (req, res) => {
         library_email,
     } = req.body;
     console.log(updatedDetails);
-  
-    try {
-      const result = await librariesModel.updateLibraryDetails(libraryId, updatedDetails);
-  
-      if (result && result.success) {
-        res.status(200).json({ message: 'Library details updated successfully' });
-      } else {
-        res.status(404).json({ error: 'Library not found or no changes applied' });
+    //req.user && req.user.library && req.user.library.id === libraryId
+    //req.user && req.user.library_id == libraryId
+    if (req.user && req.user.library && req.user.library.id === libraryId) {
+      try {
+        const result = await librariesModel.updateLibraryDetails(libraryId, updatedDetails);
+    
+        if (result && result.success) {
+          res.status(200).json({ message: 'Library details updated successfully' });
+        } else {
+          res.status(404).json({ error: 'Library not found or no changes applied' });
+        }
+      } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal Server Error' });
       }
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ error: 'Internal Server Error' });
+    } else {
+      res.status(403).json({error: 'Forbidden: Insufficient permissions for this library'})
     }
   });
   
