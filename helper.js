c4834ee388275f089b0bae327377ee5a1ece74a0
briankmbuguak <briankmbuguak@gmail.com>
2024-01-13 17:03:36 +0300

    added stringify nested objects function

--- helper.js
+++ helper.js
@@ -9,7 +9,18 @@ function emptyOrRows(rows) {
     return rows;
 }
 
+function stringifyWithNestedObject(obj) {
+    return JSON.stringify(obj, (key, value) => {
+      if (typeof value === 'object' && value !== null) {
+        // Return only specific properties of the nested object
+        return { nestedObject: value };
+      }
+      return value;
+    }, 2);
+  }
+
 module.exports = {
     getOffset,
-    emptyOrRows
+    emptyOrRows,
+    stringifyWithNestedObject
 }
